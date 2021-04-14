e5f99b2ce085d83eb21896eb4d0ec44cb8ac9fa5
Ramya Deshidi <rdeshidi@in.ibm.com>
2021-04-14 09:33:38 +0530

    Created DAL MVC using stored procedures.

---
+++ Controllers/SP_CUSTOMERController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using projectAPI.Details;
+using projectAPI.Models;
+
+
+
+
+
+namespace projectAPI.Controllers
+{
+    public class SP_CUSTOMERController : Controller
+
+    {
+        MVC_CURD_DAL dbdetails = new MVC_CURD_DAL();
+
+        // GET: SP_CUSTOMER
+        public ActionResult Index()
+        {
+            List<Customer> customerList = dbdetails.GetAllCustomers().ToList();
+            return View(customerList);
+        }
+
+        // GET: SP_CUSTOMER/Details/5
+        public ActionResult Details(int CustNo)
+        {
+            if (CustNo == null) 
+            {
+                return HttpNotFound();
+            }
+
+            return View();
+        }
+
+        // GET: SP_CUSTOMER/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SP_CUSTOMER/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SP_CUSTOMER/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SP_CUSTOMER/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SP_CUSTOMER/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SP_CUSTOMER/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Details/MVC_CURD_DAL.cs
+++ Details/MVC_CURD_DAL.cs
@@ -37,5 +37,68 @@ namespace projectAPI.Details
             }
             return customerList;
         }
+
+        public void CreateCustomer(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_CreateCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustName", customer.CustName);
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                cmd.Parameters.AddWithValue("@City", customer.City);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+            }
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_UpdateCustomer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustNo", customer.CustNo);
+
+                cmd.Parameters.AddWithValue("@CustName", customer.CustName);
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                cmd.Parameters.AddWithValue("@City", customer.City);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+            }
+        }
+        public Customer GetCustomerByID(int? Custno)
+
+        {
+            var customer = new Customer();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetCustomerbyId", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@CutNo", Custno);
+                con.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+                while (r.Read())
+                {
+                    customer.CustNo = Convert.ToInt32(r["custno"].ToString());
+                    customer.CustName = r["custname"].ToString();
+                    customer.Email = r["email"].ToString();
+                    customer.City = r["city"].ToString();
+                    customer.Phone = r["phone"].ToString();
+                }
+                con.Close();
+
+            }
+            return customer;
+        }
+
     }
+  
 }
